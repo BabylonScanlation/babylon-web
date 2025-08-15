a6d2885de864e2602fee298c09855afb2886554b
Linxurs <babylonscanlation@github.com>
2025-08-15 17:23:03 -0300

    feat: Usar variables de entorno para Firebase client config

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -7,9 +7,9 @@ import {
 } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAee_aheDnkdht9I_eagReWAP25-IaMPtY',
+  apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
   authDomain: 'babylon-scanlation-users.firebaseapp.com',
-  projectId: 'babylon-scanlation-users',
+  projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: 'babylon-scanlation-users.firebasestorage.app',
   messagingSenderId: '1091956230989',
   appId: '1:1091956230989:web:6184d7f256976c1e570504',
